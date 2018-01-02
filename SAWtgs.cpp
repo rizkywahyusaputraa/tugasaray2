cc41f1fdbe6d8da66a05c69d610163abd06b63ad
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2018-01-02 23:24:34 +0800

    membuat hasil akhir

--- SAWtgs.cpp
+++ SAWtgs.cpp
@@ -68,38 +68,8 @@ double ha1,ha2,h3;
 
 	
 	
-	RWa3c1 = Ra3c1*w1;
-	RWa3c2 = Ra3c2*w2;
-	RWa3c3 = Ra3c3*w3;
-	RWa3c4 = Ra3c4*w4;
-	RWa3c5 = Ra3c5*w5;
 	
-	// PENJUMLAHAN HASIL R * W	
-	ha1=RWa1c1+RWa1c2+RWa1c3+RWa1c4+RWa1c5;
-	ha2=RWa2c1+RWa2c2+RWa2c3+RWa2c4+RWa2c5;
-	ha3=RWa3c1+RWa3c2+RWa3c3+RWa3c4+RWa3c5;
-		
-	// MENAMPILKAN NILAI
-	cout << "MATRIKS X :"<<endl;
-	cout << a1c1 <<" | "<<a1c2<<" | "<<a1c3<<" | "<<a1c4<<" | "<<a1c5<<endl;
-	cout << a2c1 <<" | "<<a2c2<<" | "<<a2c3<<" | "<<a2c4<<" | "<<a2c5<<endl;
-	cout << a3c1 <<" | "<<a3c2<<" | "<<a3c3<<" | "<<a3c4<<" | "<<a3c5<<endl;
-	cout << endl;
-	cout << "MATRIKS R :"<<endl;
-	cout << Ra1c1 <<" | "<<Ra1c2<<" | "<<Ra1c3<<" | "<<Ra1c4<<" | "<<Ra1c5<<endl;
-	cout << Ra2c1 <<" | "<<Ra2c2<<" | "<<Ra2c3<<" | "<<Ra2c4<<" | "<<Ra2c5<<endl;
-	cout << Ra3c1 <<" | "<<Ra3c2<<" | "<<Ra3c3<<" | "<<Ra3c4<<" | "<<Ra3c5<<endl;
-	cout << endl;
-	cout << "BOBOT W :"<<endl;
-	cout << w1 <<" | "<<w2<<" | "<<w3<<" | "<<w4<<" | "<<w5<<endl;
-	cout << endl;
-	cout << "MATRIKS R * W :"<<endl;
-	cout << RWa1c1 <<" | "<<RWa1c2<<" | "<<RWa1c3<<" | "<<RWa1c4<<" | "<<RWa1c5<<endl;
-	cout << RWa2c1 <<" | "<<RWa2c2<<" | "<<RWa2c3<<" | "<<RWa2c4<<" | "<<RWa2c5<<endl;
-	cout << RWa3c1 <<" | "<<RWa3c2<<" | "<<RWa3c3<<" | "<<RWa3c4<<" | "<<RWa3c5<<endl;
-	cout << endl;
-	cout << "HASIL AKHIR  :"<<endl;
-	cout << "Nilai A1 : "<<ha1<<endl;
-	cout << "Nilai A2 : "<<ha2<<endl;
-	cout << "Nilai A3 : "<<ha3<<endl;
-}
+	
+	// PENJUMLAHAN HASIL AKHIR	
+		cout << "HASIL AKHIR  :"<<endl;
+	for(int i=0;i<3;i++)
